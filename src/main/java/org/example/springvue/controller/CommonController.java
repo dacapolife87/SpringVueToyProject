5eb8d7cfbcb16e5acca52962402e14183e86ae5a
dhtpdns <yd1505@gmail.com>
2021-01-16 21:54:57 +0900

    @ControllerAdvice 추가

--- src/main/java/org/example/springvue/controller/CommonController.java
+++ src/main/java/org/example/springvue/controller/CommonController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @Controller
+@ControllerAdvice
 public class CommonController implements ErrorController {
 
 //    @GetMapping(value = "/helloVue")
